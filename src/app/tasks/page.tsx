1ded51f815d8f66fbe750d8a64624fda1bb4f4b1
Manisha Mardi <mardimanisha101@gmail.com>
2025-07-13 15:58:30 +0530

    `Added @radix-ui/react-checkbox dependency and removed unused components and data files`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,12 +1,1 @@
 
-import AppLayout from '@/components/layout/AppLayout'
-import TaskContent from '@/components/tasks/TaskContent'
-import React from 'react'
-
-export default function page() {
-    return (
-        <AppLayout title="Tasks" badge="✅ Task Manager">
-            <TaskContent />
-        </AppLayout>
-    )
-}
--- src/components/tasks/TaskContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TaskContent() {
-    return (
-        <div>
-            Task Content.
-        </div>
-    )
-}
