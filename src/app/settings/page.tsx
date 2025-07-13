df9b705512a0f0945a99d0cd4adcf94a1d01f26b
Manisha Mardi <mardimanisha101@gmail.com>
2025-07-13 17:44:50 +0530

    `Refactor settings page: Replace Sidebar with AppLayout and add SettingsContent component`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,11 +1,12 @@
 
-import Sidebar from '@/components/layout/Sidebar'
+import AppLayout from '@/components/layout/AppLayout'
+import SettingsContent from '@/components/settings/SettingsContent'
 import React from 'react'
 
 export default function page() {
     return (
-        <div>
-            <Sidebar />
-        </div>
+        <AppLayout title='Settings' badge='⚙️ Settings'>
+            <SettingsContent />
+        </AppLayout>
     )
 }
