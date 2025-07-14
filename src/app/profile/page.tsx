be01fa40f66519c0eceb684cbff61a92b40bc314
Manisha Mardi <mardimanisha101@gmail.com>
2025-07-14 10:48:08 +0530

    `Refactor profile page to use AppLayout and ProfileContent components`
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,12 @@
 
-import Sidebar from '@/components/layout/Sidebar'
+import AppLayout from '@/components/layout/AppLayout'
+import ProfileContent from '@/components/profile/ProfileContent'
 import React from 'react'
 
 export default function page() {
     return (
-        <div>
-            <Sidebar />
-        </div>
+        <AppLayout title='Profile' badge='👤 Profile'>
+            <ProfileContent />
+        </AppLayout>
     )
 }
